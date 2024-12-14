12585382f812ddd5bd48b25f94282f83393f7e38
Emanuel <123523520+emanueldias01@users.noreply.github.com>
2024-12-14 17:39:14 -0300

    criacao das rotas

--- route/routes.go
+++ route/routes.go
@@ -1,8 +1,15 @@
 package route
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/emanueldias01/todolist/controller"
+	"github.com/gin-gonic/gin"
+)
 
 func HandleRequest(){
 	r := gin.Default()
 	defer r.Run()
+
+	r.POST("/createTask", controller.CreateTask)
+	r.GET("/:id", controller.FindTaskById)
+	r.GET("/all", controller.FindAllTasks)
 }
